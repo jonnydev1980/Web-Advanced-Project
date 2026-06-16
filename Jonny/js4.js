c8a871b49c4ef60829c26a45497b4cded549c128
Blini99 <blinhashani094@gmail.com>
2026-06-16 14:05:51 +0200

    FINAL

--- Jonny/js4.js
+++ Jonny/js4.js
@@ -12,3 +12,36 @@ function submitForm(event) {
     alert("Please fill all fields!");
   }
 }
+/* Counter */
+
+let count = 0;
+
+let reserved = setInterval(() => {
+
+  count++;
+
+  document.getElementById("counter").innerText = count;
+
+  if(count >= 150){
+    clearInterval(reserved);
+  }
+
+}, 30);
+
+/* Rating animation */
+
+const rating =
+document.getElementById("rating");
+
+rating.addEventListener("mouseenter", () => {
+
+  rating.style.transform = "scale(1.2)";
+  rating.style.transition = ".3s";
+
+});
+
+rating.addEventListener("mouseleave", () => {
+
+  rating.style.transform = "scale(1)";
+
+});
